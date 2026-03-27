7359a9d5ec8cb82bb77818180d02df218263ce2f
Mateusz Siemek <mateusz@siemek.pl>
2026-03-27 23:49:10 +0100

    Modified class ConsoleUI

--- zadanies30632/UI/ConsoleUI.cs
+++ zadanies30632/UI/ConsoleUI.cs
@@ -19,51 +19,51 @@ namespace zadanies30632.UI
 
         public void Run()
         {
-            Console.WriteLine("=== Dodawanie sprzetu ===");
+            Console.WriteLine("--- Dodawanie sprzetu ---");
             _rentalService.AddEquipment(new Laptop("Dell XPS", 16, "Intel i7"));
             _rentalService.AddEquipment(new Laptop("Lenovo ThinkPad", 8, "Intel i5"));
             _rentalService.AddEquipment(new Projector("Epson EB", 1080, true));
             _rentalService.AddEquipment(new Camera("Canon EOS", 24, true));
             
-            Console.WriteLine("\n=== Dodawanie uzytkownikow ===");
+            Console.WriteLine("\n--- Dodawanie uzytkownikow ---");
             _rentalService.AddUser(new Student("Jan", "Kowalski"));
             _rentalService.AddUser(new Student("Anna", "Nowak"));
             _rentalService.AddUser(new Employee("Piotr", "Wisniewski"));
 
             
-            Console.WriteLine("\n=== Caly sprzet ===");
+            Console.WriteLine("\n--- Caly sprzet ---");
             _raportService.ShowAllEquipments();
             
-            Console.WriteLine("\n=== Wypozyczanie sprzetu ===");
+            Console.WriteLine("\n--- Wypozyczanie sprzetu ---");
             _rentalService.RentEquipment(1, 1, 7);
             _rentalService.RentEquipment(1, 2, 5);
 
             
-            Console.WriteLine("\n=== Proba przekroczenia limitu ===");
+            Console.WriteLine("\n--- Proba przekroczenia limitu ---");
             _rentalService.RentEquipment(1, 3, 3);
             
-            Console.WriteLine("\n=== Proba wypozyczenia niedostepnego sprzetu ===");
+            Console.WriteLine("\n--- Proba wypozyczenia niedostepnego sprzetu ---");
             _rentalService.RentEquipment(2, 1, 3);
             
-            Console.WriteLine("\n=== Oznaczenie sprzetu jako niedostepny ===");
+            Console.WriteLine("\n--- Oznaczenie sprzetu jako niedostepny ---");
             _rentalService.MarkUnavailable(4);
 
             
-            Console.WriteLine("\n=== Zwrot w terminie ===");
+            Console.WriteLine("\n--- Zwrot w terminie ---");
             _rentalService.ReturnEquipment(1);
             
-            Console.WriteLine("\n=== Zwrot z opoznieniem ===");
+            Console.WriteLine("\n--- Zwrot z opoznieniem ---");
             _rentalService.GetAllRentals()[1].DueDate = DateTime.Now.AddDays(-5);
             _rentalService.ReturnEquipment(2);
             
             _rentalService.RentEquipment(3, 3, 7);
             _rentalService.GetAllRentals()[2].DueDate = DateTime.Now.AddDays(-3);
             
-            Console.WriteLine("\n=== Aktywne wypozyczenia uzytkownika 3 ===");
+            Console.WriteLine("\n--- Aktywne wypozyczenia uzytkownika 3 ---");
             _raportService.ShowAvtiveRentalForUser(3);
 
             
-            Console.WriteLine("\n=== Przeterminowane wypozyczenia ===");
+            Console.WriteLine("\n--- Przeterminowane wypozyczenia ---");
             _raportService.ShowOverdueRentals();
             
             Console.WriteLine("\n");
--- zadanies30632/obj/Debug/net10.0/zadanies30632.AssemblyInfo.cs
+++ zadanies30632/obj/Debug/net10.0/zadanies30632.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zadanies30632")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61231e64b619ec07612f22a2ade19cd64320495b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff7764bf4dcfc9420c4347428007398a3851539")]
 [assembly: System.Reflection.AssemblyProductAttribute("zadanies30632")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zadanies30632")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
